166ad333e953c181001995b6dfb2c021fbdd3eed
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2025-03-07 19:36:58 -0300

    cad v1 em andamento

---
+++ main.js
@@ -0,0 +1,136 @@
+console.log("Electron - Processo principal")
+
+// importação dos recursos do framework
+// app (aplicação)
+// BrowserWindow (criação da janela)
+// nativeTheme está relacionado ao tema (claro ou escuro)
+// Menu (definir um menu personalizado)
+// shell (acessar links externos no navegador padrão)
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
+
+// janela principal
+let win
+const createWindow = () => {
+  // definindo o tema da janela claro ou escuro
+  nativeTheme.themeSource = 'light'
+  win = new BrowserWindow({
+    width: 1010,
+    height: 720,
+    //frame: false, // totem de pedido
+    //resizable: false, // retira o redimensionamento
+    //minimizable: false, // retira a opção de minimizar
+    //closable: false, // retira a opção close
+    //autoHideMenuBar: true // esconder o menu
+  })
+
+  // carregar o menu personalizado
+  // ATENÇÃO: Antes importar o recurso Menu
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
+  // carregar o documento html na janela
+  win.loadFile('./src/views/index.html')
+}
+
+// janela sobre
+let about
+function aboutWindow() {
+  nativeTheme.themeSource='light'
+  // obter a janela principal
+  const mainWindow = BrowserWindow.getFocusedWindow()
+  // validação (se existir a janela principal)
+  if (mainWindow) {
+    about = new BrowserWindow({
+      width: 320,
+      height: 280,
+      autoHideMenuBar: true,
+      resizable: false,
+      minimizable: false,
+      // estabelecer uma relação hierárquica entre janelas
+      parent: mainWindow,
+      // criar uma janela modal (só retorna a principal quando encerrada)
+      modal: true
+    })
+  }
+  about.loadFile('./src/views/sobre.html')
+}
+
+// inicialização da aplicação (assincronismo)
+app.whenReady().then(() => {
+  createWindow()
+
+  // só ativa a janela principal se nenhuma outra estiver ativa
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// Reduzir a verbozidade de logs não críticos (devtools)
+app.commandLine.appendSwitch('log-level', '3')
+
+// se o sistema não for MAC encerrar a aplicação quando a janela for fechada
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// template do menu
+const template = [
+  {
+    label: 'Cadastro',
+    submenu: [
+      {
+        label: 'Sair',
+        accelerator: 'Alt+F4',
+        click: () => app.quit()
+      }
+    ]
+  },
+  {
+    label: 'Relatório',
+    submenu: [
+        {
+            label: 'Clientes'
+        }
+    ]
+  },
+  {
+    label: 'Ferramentas',
+    submenu: [
+      {
+        label: 'Aplicar zoom',
+        role: 'zoomIn'
+      },
+      {
+        label: 'Reduzir zoom',
+        role: 'zoomOut'
+      },
+      {
+        label: 'Restaurar zoom padrão',
+        role: 'resetZoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'DevTools',
+        role: 'toggleDevTools'
+      }
+    ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'Repositório',
+        click: () => shell.openExternal('https://github.com/clxsilva/cadastro')
+      },
+      {
+        label: 'Sobre',
+        click: () => aboutWindow()
+      }
+    ]
+  }
+]
